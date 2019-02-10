188b0154ec79ef3231746d47fc2e51f2342fb3a5
Akshit Khurana <akhurana@housecanary.com>
2019-02-10 14:51:48 -0800

    Not quite working double_dqn

--- spinup/__init__.py
+++ spinup/__init__.py
@@ -2,6 +2,7 @@
 from spinup.algos.ddpg.ddpg import ddpg
 from spinup.algos.dqn.dqn import dqn
 from spinup.algos.dueling_dqn.dueling_dqn import dueling_dqn
+from spinup.algos.double_dqn.double_dqn import double_dqn
 from spinup.algos.ppo.ppo import ppo
 from spinup.algos.sac.sac import sac
 from spinup.algos.td3.td3 import td3
